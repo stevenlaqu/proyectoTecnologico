4e9e4d7a387d6c340ae387482c75ca993e56e54b
steven <stevenlaqu@gmail.com>
2019-11-25 19:03:30 -0500

    asasas

--- servicesrasp/app/bdconfig/Config.php
+++ servicesrasp/app/bdconfig/Config.php
@@ -4,3 +4,9 @@ define ('CONNECTION_USER','root');
 define ('CONNECTION_PASSWORD','');
 define ('CONNECTION_BD','plantitas');
 define ('CONNECTION_PORT',3306);
+
+/*define ('CONNECTION_HOST','127.0.0.1');
+define ('CONNECTION_USER','plantita');
+define ('CONNECTION_PASSWORD','plantita');
+define ('CONNECTION_BD','plantitas');
+define ('CONNECTION_PORT',3306);*
